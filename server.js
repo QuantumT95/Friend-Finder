32ff0e295cd79c840c2c42cb3f7addd29506b9ab
QuantumT95 <tran.charlie@ymail.com>
2017-09-25 15:01:01 -0500

    added basic files

---
+++ server.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "friend-finder"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
